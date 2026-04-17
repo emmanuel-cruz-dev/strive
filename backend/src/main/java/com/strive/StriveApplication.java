7d8bc91af6bce83d2c5742ebd45dcc1bc016db82
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-04-17 09:14:52 -0300

    chore: adds java to backend/

---
+++ backend/src/main/java/com/strive/StriveApplication.java
@@ -0,0 +1,13 @@
+package com.strive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StriveApplication.class, args);
+	}
+
+}

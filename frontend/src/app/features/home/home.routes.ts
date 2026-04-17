e59ad1b1b4abfdfc0ee1f09ec464f6691d16470a
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-04-17 09:42:01 -0300

    feat: adds home.routes.ts to frontend/src/app/features/home/

---
+++ frontend/src/app/features/home/home.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+
+import { Home } from './pages/home';
+
+export const HOME_ROUTES: Routes = [
+  {
+    path: '',
+    component: Home,
+    title: 'Inicio',
+  },
+];

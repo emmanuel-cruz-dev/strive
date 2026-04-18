a6e505c3d5f837fc5fedf0acdc02b74e1c63e395
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-04-18 11:47:41 -0300

    feat: adds dashboard-layout files to frontend/src/app/layouts/dashboard-layout/

---
+++ frontend/src/app/layouts/dashboard-layout/dashboard-layout.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-layout',
+  imports: [],
+  templateUrl: './dashboard-layout.html',
+})
+export class DashboardLayout {}

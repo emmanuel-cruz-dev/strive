d65c96ff96f8554332c3a73637774c9ac89828c6
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-04-20 11:22:16 -0300

    feat: adds functions.routes.ts to frontend/src/app/features/functions/

---
+++ frontend/src/app/features/functions/functions.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+
+import { Functions } from './pages/functions';
+
+export const FUNCTIONS_ROUTES: Routes = [
+  {
+    path: '',
+    component: Functions,
+    title: 'Funciones',
+  },
+];

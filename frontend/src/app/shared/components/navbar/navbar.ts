9527991e856f3e30b35cf0d72c5961694788346a
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-04-20 13:46:35 -0300

    feat: adds icons, attributes and methods to frontend/src/app/shared/components/navbar/navbar.ts

--- frontend/src/app/shared/components/navbar/navbar.ts
+++ frontend/src/app/shared/components/navbar/navbar.ts
@@ -1,8 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzDrawerModule } from 'ng-zorro-antd/drawer';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink, NzMenuModule, NzButtonModule, NzIconModule, NzDrawerModule],
   templateUrl: './navbar.html',
 })
-export class Navbar {}
+export class Navbar {
+  isMobile = signal(window.innerWidth < 768);
+  drawerVisible = signal(false);
+
+  @HostListener('window:resize')
+  onResize(): void {
+    this.isMobile.set(window.innerWidth < 768);
+    if (!this.isMobile()) this.drawerVisible.set(false);
+  }
+
+  openDrawer(): void {
+    this.drawerVisible.set(true);
+  }
+  closeDrawer(): void {
+    this.drawerVisible.set(false);
+  }
+}

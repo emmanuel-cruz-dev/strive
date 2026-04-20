38b7b4c0a3c9f9ce28b7b66681c6ffceb3ea70ba
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-04-20 10:47:53 -0300

    feat: adds profile-layout files to frontend/src/app/layouts/profile-layout/

---
+++ frontend/src/app/layouts/profile-layout/profile-layout.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-layout',
+  imports: [],
+  templateUrl: './profile-layout.html',
+})
+export class ProfileLayout {}

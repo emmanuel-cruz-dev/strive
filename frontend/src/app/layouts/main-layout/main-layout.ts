e18bde7c26b50bae07eddba45b8ea3ce6308aa24
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-04-21 10:36:42 -0300

    feat: adds Navbar and Footer to main-layout files in frontend/src/app/layouts/main-layout/

--- frontend/src/app/layouts/main-layout/main-layout.ts
+++ frontend/src/app/layouts/main-layout/main-layout.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+import { Navbar, Footer } from '../../shared';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [],
+  imports: [RouterOutlet, Navbar, Footer],
   templateUrl: './main-layout.html',
 })
 export class MainLayout {}

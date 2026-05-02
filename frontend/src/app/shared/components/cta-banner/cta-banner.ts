a116f78b73f214db08983c12cc52142c13be0ced
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-05-02 14:39:57 -0300

    feat: adds cta-banner files to frontend/src/app/shared/components/cta-banner/

---
+++ frontend/src/app/shared/components/cta-banner/cta-banner.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cta-banner',
+  imports: [],
+  templateUrl: './cta-banner.html',
+})
+export class CtaBanner {}
--- frontend/src/app/shared/components/index.ts
+++ frontend/src/app/shared/components/index.ts
@@ -1,2 +1,3 @@
 export * from './navbar/navbar';
 export * from './footer/footer';
+export * from './cta-banner/cta-banner';

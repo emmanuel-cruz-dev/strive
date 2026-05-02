f32538b270c7d76eec9f7d762a744ec2edb65008
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-05-02 14:37:56 -0300

    feat: adds withInMemoryScrolling to frontend/src/app/app.config.ts

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -3,7 +3,7 @@ import {
   provideBrowserGlobalErrorListeners,
   provideZonelessChangeDetection,
 } from '@angular/core';
-import { provideRouter, TitleStrategy } from '@angular/router';
+import { provideRouter, withInMemoryScrolling, TitleStrategy } from '@angular/router';
 import { en_US, provideNzI18n } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
@@ -18,7 +18,12 @@ registerLocaleData(en);
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
+    provideRouter(
+      routes,
+      withInMemoryScrolling({
+        scrollPositionRestoration: 'top',
+      })
+    ),
     provideZonelessChangeDetection(),
     provideTranslateService({
       loader: provideTranslateHttpLoader({

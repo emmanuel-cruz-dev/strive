735ecea0956ca4cba7d4b8aa89620f7bb2a42ed7
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-05-03 14:22:24 -0300

    feat: adds HeroSection and CtaBanner to home files in frontend/src/app/features/home/pages/

--- frontend/src/app/features/home/pages/home.ts
+++ frontend/src/app/features/home/pages/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
+import { HeroSection } from '../components/hero-section/hero-section';
+import { CtaBanner } from '../../../shared';
+
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [HeroSection, CtaBanner],
   templateUrl: './home.html',
 })
-export class Home {
-
-}
+export class Home {}

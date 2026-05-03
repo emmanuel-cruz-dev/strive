ba45101fbfbb102150b9f2cdbd9e3dd53ccba6e2
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-05-03 14:21:10 -0300

    feat: adds contact-information files to frontend/src/app/features/contact/components/contact-information/

---
+++ frontend/src/app/features/contact/components/contact-information/contact-information.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-information',
+  imports: [],
+  templateUrl: './contact-information.html',
+})
+export class ContactInformation {}

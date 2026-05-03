b129406b0a9e0363d1fa6e29a9ba24e318a4d7bf
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-05-03 14:23:11 -0300

    feat: adds ContactForm to contact files in frontend/src/app/features/contact/pages/

--- frontend/src/app/features/contact/pages/contact.ts
+++ frontend/src/app/features/contact/pages/contact.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 
+import { ContactForm } from '../components/contact-form/contact-form';
+
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [ContactForm],
   templateUrl: './contact.html',
 })
 export class Contact {}
